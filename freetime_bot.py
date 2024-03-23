25f682f43cfe2ddcf1e06416a035f6fec5aee6d3
Leonty <krokxa228@gmail.com>
2024-03-23 20:37:57 +0500

    Создание примерного макета выбора действий

--- freetime_bot.py
+++ freetime_bot.py
@@ -3,19 +3,15 @@ from telebot import types
 import data
 
 bot = telebot.TeleBot(data.token)
+waiting_for_groupname = {}
 
-def send_actions_keyboard(chat_id):
-    keyboard = types.InlineKeyboardMarkup()
-    sendInfoButton = types.InlineKeyboardButton(text="INFO", callback_data="Info")
-    createGroupButton = types.InlineKeyboardButton(text="Новая группа", callback_data="CreateGroup")
-    intervalsEditingButton = types.InlineKeyboardButton(text="Доступность", callback_data="Intervals")
-    keyboard.add(intervalsEditingButton, createGroupButton, sendInfoButton)
-    bot.send_message(chat_id, "Выберите действие:", reply_markup=keyboard)
-@bot.message_handler(commands=['start']) 
+
+@bot.message_handler(commands=['start'])
 def handle_start(message): 
     # Разделяем команду и параметры 
     args = message.text.split() 
-    # Проверяем, есть ли параметр после /start 
+    # Проверяем, есть ли параметр после /start
+    # Реализовать проверку регистрации пользователя. Если не зареган - зарегать.
     if len(args) > 1: 
         groupname = args[1] # Получаем параметр
 
@@ -25,16 +21,26 @@ def handle_start(message):
         keyboard.add(yesButton, noButton)
         bot.send_message(message.chat.id, f"Ты хочешь вступить в группу {groupname}?", reply_markup=keyboard)
 
-        # Замыкание для передачик параметра группы в обработчик колбэк запросов
+
+        # Замыкание для передачи параметра группы в обработчик колбэк запросов
         bot.callback_query_handler(func=lambda call: call.data in ["Yes", "No"])(create_callback_handler(groupname))
 
-        send_actions_keyboard(message.chat.id)
-    else: 
+        # send_actions_keyboard(message.chat.id)
+    else:
         bot.reply_to(message, f"Привет, я бот для определения общего времени для группы людей.")
         if ...: # Если пользователь зарегестрирован в БД, то выводить доп.информацию не нужно
             send_help(message)
 
+
         send_actions_keyboard(message.chat.id)
+
+def send_actions_keyboard(chat_id):
+    keyboard = types.InlineKeyboardMarkup()
+    sendInfoButton = types.InlineKeyboardButton(text="INFO", callback_data="Info")
+    createGroupButton = types.InlineKeyboardButton(text="Новая группа", callback_data="CreateGroup")
+    intervalsEditingButton = types.InlineKeyboardButton(text="Доступность", callback_data="Intervals")
+    keyboard.add(intervalsEditingButton, createGroupButton, sendInfoButton)
+    bot.send_message(chat_id, "Выберите действие:", reply_markup=keyboard)
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
@@ -46,24 +52,54 @@ def create_callback_handler(groupname):
             # Написать логику проверки такой группы в базе
             # Также проверки возможно этот пользователь уже в этой группе и тд
             bot.send_message(call.message.chat.id, f"Вы присоединились к группе - {groupname}.")
+            send_actions_keyboard(call.message.chat.id)
         elif call.data == "No":
             bot.send_message(call.message.chat.id, f"Вы отклонили предложение на вступление в группу -  {groupname}.")
+            send_actions_keyboard(call.message.chat.id)
     return handle_callback
 
 @bot.callback_query_handler(func=lambda call: call.data == "Info")
 def handle_join_group_callback(call):
     # Обработка нажатия на кнопку "info"
-    bot.send_message(call.message.chat.id, "Обработка действия 'info'")
+    bot.send_message(call.message.chat.id, "Список групп с ссылками, а также какая либо служебная информаци")
 
 @bot.callback_query_handler(func=lambda call: call.data == "CreateGroup")
 def handle_create_group_callback(call):
-    # Обработка нажатия на кнопку "Создать новую группу"
-    bot.send_message(call.message.chat.id, "Обработка действия 'Создать новую группу'")
+    keyboard = types.InlineKeyboardMarkup()
+    cancelButton = types.InlineKeyboardButton(text="Отмена", callback_data="Cancel")
+    keyboard.add(cancelButton)
+    bot.send_message(call.message.chat.id, "Напишите название группы, которую вы хотите создать:", reply_markup=keyboard)
+    # Устанавливаем состояние ожидания названия группы
+    waiting_for_groupname[call.message.chat.id] = True
+
+@bot.message_handler(func=lambda message: waiting_for_groupname.get(message.chat.id))
+def handle_groupname_input(message):
+    groupname = message.text
+
+    if groupname_exists(groupname):
+        bot.send_message(message.chat.id, "Группа с таким названием уже существует. Попробуйте еще раз")
+        return
+
+    # Если название прошло все проверки, можно продолжать создание группы
+    # Удаляем состояние ожидания названия группы
+    waiting_for_groupname.pop(message.chat.id)
+
+    # Логика создания ссылки а также шифрование
+
+
+def groupname_exists(groupname):
+    return True # логика проверки есть ли группа в базе с таким названием
+@bot.callback_query_handler(func=lambda call: call.data == "Cancel")
+def handle_cancel_callback(call):
+    # Отменяем текущее действие
+    bot.send_message(call.message.chat.id, "Создание новой группы отменено")
+    # Удаляем состояние ожидания названия группы
+    waiting_for_groupname.pop(call.message.chat.id)
 
 @bot.callback_query_handler(func=lambda call: call.data == "Intervals")
 def handle_info_callback(call):
     # Обработка нажатия на кнопку "Intervals"
-    bot.send_message(call.message.chat.id, "Обработка действия 'intervals'")
+    bot.send_message(call.message.chat.id, "Открытие web приложения")
 
 
 # Запуск бота
